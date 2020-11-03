ae8626e8e82db09711b719f8a35d9fe5518523d7
Marcin <marcin.nieznaj@gmail.com>
2020-11-03 15:33:42 +0100

    heroku port fix

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT);
