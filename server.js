51f3372de4bf136e88934ca31acf88a9b65fb62f
Marcin <marcin.nieznaj@gmail.com>
2020-11-04 12:11:34 +0100

    naigation fix

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('*', function(req, res) {  
+  res.redirect('https://' + req.headers.host + req.url);
+});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
